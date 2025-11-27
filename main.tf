008f607ca6c027389e9fec9487fd833ad2aa6785
Chetan K <chetan1702@hotmail.com>
2025-11-27 18:20:50 +0530

    updating azure public IP deployment type

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ resource "azurerm_public_ip" "public_ip" {
   name                = "${each.key}-pip"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "static"
 }
 
 # NIC
