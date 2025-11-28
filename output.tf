066f9c6f3dd9032f1cf414109ec80755ee599b6c
Chetan K <chetan1702@hotmail.com>
2025-11-29 02:45:17 +0530

    added RG creation code on locally

--- output.tf
+++ output.tf
@@ -1,25 +1,5 @@
-output "public_ip_address" {
-  description = "Public IP address of each VM"
-  value = {
-    for key, pip in azurerm_public_ip.public_ip :
-    key => pip.ip_address
-  }
+
+output "rg_name" {
+  value = azurerm_resource_group_rg.name
 }
 
-output "vm_public_ips" {
-  value = {
-    for vm, nic in azurerm_network_interface.nic :
-    vm => azurerm_public_ip.public_ip[vm].ip_address
-  }
-}
-
-output "admin_username" {
-  description = "The administrator username."
-  value       = var.admin_username
-}
-
-output "admin_password" {
-  description = "The administrator password (sensitive)."
-  value       = var.admin_password
-  sensitive   = true
-}
